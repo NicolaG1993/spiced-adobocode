d8182c64349ad31c57a050a40cd2925a6f4b896a
Nicola Gaioni <nicog.designer@gmail.com>
2020-12-04 18:44:35 +0100

    what i did on 04.12

--- ex04-12.js
+++ ex04-12.js
@@ -20,3 +20,32 @@ var rect = new Rectangle(4, 5);
 rect.getArea();
 
 //EXERCISE 2
+function invertCase(str) {
+    // var newStr = str;
+    str.split("").filter(function (newStr) {
+        if (newStr.charCodeAt(0) >= 65 && newStr.charCodeAt(0) <= 90) {
+            return concat(newStr.toLowerCase());
+        }
+    });
+
+    //  if () {}
+    // return str.toUpperCase || str.toLowerCase;
+}
+
+function invertCase(str) {
+    var splitStr = str.split();
+
+    for (var i = 0; i < splitStr.length; i++) {
+        if (splitStr[i] === splitStr[i].toUpperCase()) {
+            return splitStr[i].toLowerCase();
+        } else {
+            return splitStr[i].toUpperCase();
+        }
+        return spliStr.join();
+    }
+}
+
+console.log(invertCase("hey"));
+console.log(invertCase("HEY"));
+console.log(invertCase("Hey Nicola Gaioni"));
+console.log(invertCase("1969 $ & % #"));
