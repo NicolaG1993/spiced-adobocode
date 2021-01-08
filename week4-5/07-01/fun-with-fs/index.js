ea353197e8ae40c34d5beacc01049cb89992c107
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-08 01:30:13 +0100

    07.01

--- week4-5/07-01/fun-with-fs/index.js
+++ week4-5/07-01/fun-with-fs/index.js
@@ -5,38 +5,43 @@ const myPath = __dirname;
 
 //PART 1
 
-const logSizes = fs.readdir(myPath, { withFileTypes: true }, (err, content) => {
-    if (err) {
-        console.log("error!", err);
-        return;
-    }
-    for (let i = 0; i < content.length; i++) {
-        //console.log("content[i]: ", content[i]);
-        //console.log("isFile? ", content[i].isFile());
-        //console.log("isDirectory? ", content[i].isDirectory());
-
-        if (content[i].isFile()) {
-            fs.stat(myPath, (err, stats) => {
-                //console.log("stats: ", stats);
-                console.log(
-                    chalk.bgBlueBright(
-                        `${__dirname + __filename}: ${stats.size}`
-                    )
-                );
-            });
-        } else if (content[i].isDirectory()) {
+const logSizes = (x) => {
+    //passo x invece di myPath perché non voglio sia un valore fisso
+    fs.readdir(x, { withFileTypes: true }, (err, content) => {
+        if (err) {
+            console.log("error!", err);
+            return;
         }
-    }
-});
+        for (let i = 0; i < content.length; i++) {
+            //console.log("content[i]: ", content[i]);
+            //console.log("isFile? ", content[i].isFile());
+            //console.log("isDirectory? ", content[i].isDirectory());
+            let path = x + `/${content[i].name}`; //creo un nuovo path che aggiunge il nome del file attuale a myPath
+            //console.log("x: ", x);
+            //console.log("path: ", path);
+
+            if (content[i].isFile()) {
+                fs.stat(path, (err, stats) => {
+                    if (err) {
+                        console.log("error!", err);
+                        return;
+                    }
+                    //console.log("stats: ", stats);
+                    console.log(chalk.bgBlueBright(`${path}: ${stats.size}`));
+                });
+            } else if (content[i].isDirectory()) {
+                //console.log("content[i]: ", content[i].name);
+                //console.log("dirPath: ", dirPath);
+                logSizes(path);
+            }
+        }
+    });
+};
+
+logSizes(myPath);
 
 //PART 2
 
 //const part2Content = fs.readdirSync();
 
 //PART 3
-
-// For every file it should call fs.stat to determine the file's size. Once the size is known,
-//it should log the path to the file followed by a colon and the size of the file.
-
-// For every directory, it should call itself and pass the path to the directory so that
-//the sizes of all the files contained in the directory will be logged.
