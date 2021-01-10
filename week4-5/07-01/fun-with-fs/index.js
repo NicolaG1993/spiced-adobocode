e401d60a6af4f320db9622b4c8a40dfa46fe3933
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-10 04:31:18 +0100

    week 5

--- week4-5/07-01/fun-with-fs/index.js
+++ week4-5/07-01/fun-with-fs/index.js
@@ -5,38 +5,38 @@ const myPath = __dirname;
 
 //PART 1
 
-// const logSizes = (x) => {
-//     //passo x invece di myPath perché non voglio sia un valore fisso
-//     fs.readdir(x, { withFileTypes: true }, (err, content) => {
-//         if (err) {
-//             console.log("error!", err);
-//             return;
-//         }
-//         for (let i = 0; i < content.length; i++) {
-//             //console.log("content[i]: ", content[i]);
-//             //console.log("isFile? ", content[i].isFile());
-//             //console.log("isDirectory? ", content[i].isDirectory());
-//             let path = x + `/${content[i].name}`; //creo un nuovo path che aggiunge il nome del file attuale a myPath
-//             //console.log("x: ", x);
-//             //console.log("path: ", path);
+const logSizes = (x) => {
+    //passo x invece di myPath perché non voglio sia un valore fisso
+    fs.readdir(x, { withFileTypes: true }, (err, content) => {
+        if (err) {
+            console.log("error!", err);
+            return;
+        }
+        for (let i = 0; i < content.length; i++) {
+            //console.log("content[i]: ", content[i]);
+            //console.log("isFile? ", content[i].isFile());
+            //console.log("isDirectory? ", content[i].isDirectory());
+            let path = x + `/${content[i].name}`; //creo un nuovo path che aggiunge il nome del file attuale a myPath
+            //console.log("x: ", x);
+            //console.log("path: ", path);
 
-//             if (content[i].isFile()) {
-//                 fs.stat(path, (err, stats) => {
-//                     if (err) {
-//                         console.log("error!", err);
-//                         return;
-//                     }
-//                     //console.log("stats: ", stats);
-//                     console.log(chalk.bgBlueBright(`${path}: ${stats.size}`));
-//                 });
-//             } else if (content[i].isDirectory()) {
-//                 //console.log("content[i]: ", content[i].name);
-//                 //console.log("dirPath: ", dirPath);
-//                 logSizes(path);
-//             }
-//         }
-//     });
-// };
+            if (content[i].isFile()) {
+                fs.stat(path, (err, stats) => {
+                    if (err) {
+                        console.log("error!", err);
+                        return;
+                    }
+                    //console.log("stats: ", stats);
+                    console.log(chalk.bgBlueBright(`${path}: ${stats.size}`));
+                });
+            } else if (content[i].isDirectory()) {
+                //console.log("content[i]: ", content[i].name);
+                //console.log("dirPath: ", dirPath);
+                logSizes(path);
+            }
+        }
+    });
+};
 
 // logSizes(myPath);
 
