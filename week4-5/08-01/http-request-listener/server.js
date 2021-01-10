5978bbae1678aa8de1b4e4cef1fa775a2cb51964
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-10 06:36:42 +0100

    week5

--- week4-5/08-01/http-request-listener/server.js
+++ week4-5/08-01/http-request-listener/server.js
@@ -1,53 +1,56 @@
 const http = require("http");
-//console.log("http: ", http); // obj{METHODS:[], STATUS_CODES:{}, Agent: {}[]}
+
+const fs = require("fs");
+//console.log("fs: ", fs);
 
 const server = http.createServer(function (req, res) {
-    // inside here we will handle requests and send responses
     req.on("error", (error) => {
-        console.log("error on req object: ", error);
+        console.log("error on request object: ", error);
     });
     res.on("error", (error) => {
         console.log("error on response object: ", error);
     });
+    console.log("req.url: ", req.url);
+    console.log("req.headers: ", req.headers);
+    console.log("req.method: ", req.method);
 
-    // console.log("req: ", req); //obj {objs and properties}
-    // console.log("req.url: ", req.url); // /path/to/file/index.html?name=value
-    // console.log("req.headers: ", req.headers); // obj {host, connection, accept, dnt}
-    // console.log("req.method: ", req.method); // GET
+    let requests = [new Date(), req.method, req.url, req.headers["user-agent"]];
+    let requestsFormatted = "\n" + requests.join("\t");
+    fs.appendFile("requests.txt", requestsFormatted, (error) => {
+        if (error) {
+            console.log("error on appendFile request: ", error);
+        }
+    });
 
     if (req.method === "GET" || req.method === "HEAD") {
-        //console.log("req: ", req); //obj {objs and properties}
-        //console.log("req.url: ", req.url); // /path/to/file/index.html?name=value
-        //console.log("req.headers: ", req.headers); // obj {host, connection, accept, dnt}
-        console.log("req.method: ", req.method); // GET
-
         res.setHeader("Content-Type", "text/html");
         res.statusCode = 200;
 
-        if (req.method === "GET") {
-            console.log("This is a GET request!!!");
-        } else if (req.method === "HEAD") {
+        if (req.method === "HEAD") {
             console.log("This is a HEAD request!!!");
+            // ???
+        } else if (req.method === "GET") {
+            res.end(`<!doctype html>
+            <html>
+                <title>Hello World!</title>
+                <p>Hello Worlda!</p>
+            </html>`);
         }
     } else if (req.method === "POST") {
-        console.log("This is a POST request!!!");
+        let body = "";
+        req.on("data", function (chunk) {
+            body += chunk;
+        }).on("end", function () {
+            res.setHeader("Location", "/");
+            res.statusCode = 302;
+            res.end();
+        });
+    } else {
+        res.statusCode = 405;
+        res.end();
     }
-
-    let body = "";
-    req.on("data", function (chunk) {
-        body += chunk;
-    }).on("end", function () {
-        //console.log("body: ", body); // logs the entire request body // ? empty ?
-        //console.log("res: ", res); // obj{objs}
-        res.setHeader("Content-Type", "text/html"); // ?? sembra inutile
-        res.statusCode = 200; // ?? sembra inutile
-        res.end(`
-                <!doctype html>
-                <h1>Thanks! ${body} </h1>
-                `);
-    }); // return html on our browser
 });
 
 server.listen(8080, () => console.log("My server is listening..."));
 
-// http://localhost:8080/path/to/file/index.html?name=value
+// http://localhost:8080
