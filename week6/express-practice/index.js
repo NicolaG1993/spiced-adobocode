7a8d10f55b7e3ff73256dcaa05df7c3f064dfc52
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-12 20:41:02 +0100

    12.01

--- week6/express-practice/index.js
+++ week6/express-practice/index.js
@@ -1,40 +1,70 @@
 const express = require("express"); //é una funzione
 const app = express(); //un obj pieno di metodi
+const cookieParser = require("cookie-parser");
+
+app.use(express.urlencoded({ extended: false })); //this is what parse all the data
+app.use(cookieParser());
+app.use((req, res, next) => {
+    console.log(`${req.method} request was made to ${req.url}`);
+    //console.log("req.cookies: ", req.cookies);
+
+    //res.cookie("reqURL", req.url);
+    //let theCookie = req.cookies;
+
+    //console.log("theCookie: ", theCookie);
+
+    if (req.url !== "/cookies") {
+        if (req.cookies["cookies-accepted"]) {
+            console.log("Cookies are valide, u can navigate! 👽");
+            //res.redirect(theCookie.reqURL);
+            //next();
+        } else {
+            res.cookie("reqURL", req.url);
+            res.redirect("/cookies");
+        }
+    }
+    next(); //we need it to make the fn(or express?) running
+    //i dont get this part
+});
+app.use(express.static("./public")); //i dont get this part
+app.use(express.static("./projects"));
 
 app.get("/", (req, res) => {
     //req e res hanno nuovi metodi!
-    console.log("GET req was made to the / route");
-    res.send("<h1>HEY</h1>");
+    console.log("I confirm GET req was made to the / route");
+    //res.cookie("first-cookie", "AMAZING!");
+    // res.send("<h1>HEY</h1>");
+    res.sendFile(__dirname + "/index.html");
     // res.redirect("/about");
     // res.render();
     // res.json();
 });
 
-//potremmo fare tutto in one line:
-//app.get("/", (req, res) => res.send("<h1>HEY</h1>")); //un metodo invece di 4: error, set headers, code, end
+//se i cookie non sono stati accettati
+//reindirizza a /cookies //ma prima crea un valore previousLink in cookie con l'attuale url
+//user accetta -> POST request a /cookies
+//cookie salva data dello user
+//conserva data per tutto il tempo che user naviga nel sito
+//reindirizza user a link precedente di /cookies
+//se non accetta ritorna html --> accesso sito vietato
+
+/////////////////////
+/////////////////////
+app.get("/cookies", (req, res) => {
+    //console.log("req made to /cookies route", req.method, req.url);
+    res.sendFile(__dirname + "/cookies-settings.html");
+});
+/////////////////////
+/////////////////////
 
 app.get("/about", (req, res) => {
-    console.log("req made to /about route");
-    res.sendFile(__dirname + "/index.html");
+    //console.log("req made to /about route");
+    res.sendFile(__dirname + "/about.html");
 });
 
-app.get("/users/:username", (req, res) => {
-    console.log("req.params: ", req.params);
-});
-//or
 app.get("/users/:username/:postId", (req, res) => {
-    console.log("req.params: ", req.params);
-
-    //here i could get data from the DB
-
-    res.send(`
-    <h1>This is the page for user ${req.params.username}</h1>
-    <h2>This is the post for the id ${req.params.postId}</h2>
-    `);
-});
-//or
-app.get("/users/:username/:postId", (req, res) => {
-    const { username, postId } = req.params;
+    //console.log("req.params: ", req.params);
+    const { username, postId } = req.params; //req.params.username
 
     //here i could get data from the DB
 
@@ -42,6 +72,76 @@ app.get("/users/:username/:postId", (req, res) => {
     <h1>This is the page for user ${username}</h1>
     <h2>This is the post for the id ${postId}</h2>
     `);
+});
+
+app.get("/register", (req, res) => {
+    res.send(`
+<h2>Please tell us about yourself</h2>
+        <form method='POST' style="display: flex; flex-direction: column; justify-content: space-between; width: 40%; height: 50%;">
+            <input type='text' name='firstname' placeholder='First Name' autocomplete='off'>
+            <input type='text' name='lastname' placeholder='Last Name' autocomplete='off'>
+            <div>
+                <span>How old are you? </span><input type="number" name="age">
+            </div>
+            <div>
+                <input type="checkbox" name="subscribe"><span>Would you like to receive our newsletter?</span>
+            </div>
+            <button> submit </submit>
+        </form>
+    `); //if i click submit in the form its making a post req to /register
+});
+
+app.post("/register", (req, res) => {
+    console.log("post made to /register");
+    console.log("req.body: ", req.body);
+    //i need post req to send data in an obj //sensitive infos: email, name, etc
+    //we use middleware
+    res.cookie("authenticated", true);
+
+    const { firstname, lastname, age, subscribe } = req.body; // req.body.firstname, etc
+    //vengono presi dal name nel html del form, sono i valori nel nostro obj (body)
+    if (subscribe) {
+        res.send(`
+        <h1>Thanks ${firstname} ${lastname} for subscribing to the newsletter!</h1>
+        <h2>You are ${age} years old apparaently</h2>`);
+    } else {
+        res.send(`
+        <h1>What? You don't want to subscribe to the newsletter!?</h1>
+        <h2>See u in ${age} years ${firstname} ${lastname}</h2>
+        `);
+    }
+});
+
+/////////////////////
+/////////////////////
+app.post("/cookies", (req, res) => {
+    //console.log("my post req made to /cookies: ", req.body);
+    const accepted = req.body["accept-cookies"];
+    if (accepted) {
+        //res.send(`<h1>YEAH cookies accepted</h1>`);
+        res.cookie("cookies-accepted", "YEAH");
+        //res.send(`<h1>YEAH cookies accepted</h1>`);
+        console.log("COOKIE WILL REDIRECT TO: ", req.cookies.reqURL);
+        res.redirect(req.cookies.reqURL);
+        res.clearCookie(req.cookies.reqURL);
+        //console.log("req.cookies: ", req.cookies);
+    } else {
+        res.send(`<h1>NOPE cookies not accepted</h1>`);
+    }
+});
+/////////////////////
+/////////////////////
+
+app.get("/private", (req, res) => {
+    console.log("req.cookies: ", req.cookies);
+    if (req.cookies.authenticated) {
+        res.send(`
+            <h1>TOP SECRET STUFF ✋ 🛑</h1>
+            <h1>This is so secret and private</h1>
+        `);
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.listen(8080, () => console.log("Server listening on port 8080.."));
---
+++ week6/express-practice/projects/ConnectFour/script.js
@@ -0,0 +1,382 @@
+(function () {
+    var currentPlayer = "player1";
+    var preChip = $(".preChip");
+
+    var board = $(".board");
+    var slots = $(".slot");
+    var hand = $(".handCont");
+    var victory = $(".victory");
+    //console.log("slots:", slots);
+    var score1 = 0;
+    var score2 = 0;
+
+    $(".column").on("click", function (e) {
+        //console.log("target:", e.target);
+        //console.log("currentTarget:", e.currentTarget);
+        //console.log("rowX: ", $(".row3"));
+        var col = $(e.currentTarget);
+        var slotsInCol = col.children();
+        //console.log("slotsInCol :", slotsInCol);
+        //console.log("slotsInCol.length :", slotsInCol.length);
+
+        for (var i = slotsInCol.length - 1; i >= 0; i--) {
+            //console.log("slotsInCol[i] :", slotsInCol.eq(i));
+            if (
+                !slotsInCol.eq(i).hasClass("player1") &&
+                !slotsInCol.eq(i).hasClass("player2")
+            ) {
+                slotsInCol.eq(i).addClass(currentPlayer);
+
+                break;
+            }
+        }
+
+        var slotsInRow = $(".row" + i);
+
+        if (checkforVictory(slotsInCol)) {
+            gameOver("column victory");
+        } else if (checkforVictory(slotsInRow)) {
+            gameOver("row victory");
+        } else if (checkforVictory(slots)) {
+            gameOver("diagonal victory");
+        }
+
+        switchPlayer();
+        preChip.removeClass("player1" && "player2");
+        preChip.addClass(currentPlayer);
+    });
+
+    function switchPlayer() {
+        if (currentPlayer === "player1") {
+            currentPlayer = "player2";
+        } else {
+            currentPlayer = "player1";
+        }
+    }
+
+    // function switchColor() {
+    //     if (currentColor === "color1") {
+    //         currentColor = "color2";
+    //     } else {
+    //         currentColor = "color1";
+    //     }
+    // }
+
+    function checkforVictory(slots) {
+        //console.log(slots);
+        var counter = 0;
+        for (var i = 0; i < slots.length; i++) {
+            //console.log(slots.eq(i));
+            if (slots.eq(i).hasClass(currentPlayer)) {
+                counter++;
+                if (counter === 4) {
+                    //inserire condizioni per bug bordo alto
+                    //if con ogni coppia di i
+                    // if (
+                    //     slots.eq(5 || 11 || 17 || 23 || 29 || 35 || 41) &&
+                    //     slots.eq(0 || 6 || 12 || 18 || 24 || 30 || 36)
+                    // ) {return false; }
+
+                    //console.log("I won!");
+                    return true;
+                }
+            } else {
+                counter = 0;
+                //console.log("counter go back to 0");
+            }
+            if (
+                //checkDiag
+                slots.eq(i).hasClass(currentPlayer) &&
+                slots.eq(i + 5).hasClass(currentPlayer) &&
+                slots.eq(i + 10).hasClass(currentPlayer) &&
+                slots.eq(i + 15).hasClass(currentPlayer)
+            ) {
+                return true;
+                //console.log("rx Diagonal won!");
+            } else if (
+                slots.eq(i).hasClass(currentPlayer) &&
+                slots.eq(i + 7).hasClass(currentPlayer) &&
+                slots.eq(i + 14).hasClass(currentPlayer) &&
+                slots.eq(i + 21).hasClass(currentPlayer)
+            ) {
+                return true;
+                //console.log("sx Diagonal won!");
+            }
+        }
+    }
+
+    //GAME OVER - CONTINUE - RESET
+    function gameOver(str) {
+        victory.css("visibility", "visible");
+        if (currentPlayer === "player1") {
+            score1++;
+            $("#player1Score").html(score1);
+        } else {
+            score2++;
+            $("#player2Score").html(score2);
+            //localstorage? // non riesco a settare default value 0
+        }
+        $(".winner").text("🎉 The winner is " + currentPlayer + " 🎉");
+        $(".victoryStyle").text("It was a " + str);
+        console.log(str);
+    }
+
+    var playAgain = $(".playAgain");
+    var reset = $(".reset");
+
+    playAgain.on("click", function () {
+        slots.removeClass("player1");
+        slots.removeClass("player2");
+        victory.css("visibility", "hidden");
+    });
+
+    reset.on("click", function () {
+        //reset local storage
+        //clear console
+        slots.removeClass("player1");
+        slots.removeClass("player2");
+        $("#player1Score").html((score1 = 0));
+        $("#player2Score").html((score2 = 0));
+        victory.css("visibility", "hidden");
+    });
+
+    //MUOSE HOVER ANIMATION
+    board.on("mousemove", function (e) {
+        hand.css("left", e.clientX - "120" + "px");
+    });
+
+    board.on("mouseover", function (e) {
+        var arrowColumn = $(".arrowColumn");
+
+        switch (e.target) {
+            case e.currentTarget.children[0]:
+                arrowColumn.html("");
+                arrowColumn.eq(0).html("<span>▲</span>");
+                break;
+            case e.currentTarget.children[1]:
+                arrowColumn.html("");
+                arrowColumn.eq(1).html("<span>▲</span>");
+                break;
+            case e.currentTarget.children[2]:
+                arrowColumn.html("");
+                arrowColumn.eq(2).html("<span>▲</span>");
+                break;
+            case e.currentTarget.children[3]:
+                arrowColumn.html("");
+                arrowColumn.eq(3).html("<span>▲</span>");
+                break;
+            case e.currentTarget.children[4]:
+                arrowColumn.html("");
+                arrowColumn.eq(4).html("<span>▲</span>");
+                break;
+            case e.currentTarget.children[5]:
+                arrowColumn.html("");
+                arrowColumn.eq(5).html("<span>▲</span>");
+                break;
+            case e.currentTarget.children[6]:
+                arrowColumn.html("");
+                arrowColumn.eq(6).html("<span>▲</span>");
+                break;
+            default:
+                break;
+        }
+    });
+
+    // //COLOR MENU
+    var colorPicker = $(".colorPicker");
+    var colorsCont = $(".colorsCont");
+    colorPicker.on("click", function () {
+        colorsCont.toggleClass("on");
+    });
+
+    // cant make this part works
+    //var colors = $(".skin");
+    // colors.on("click", function (e) {
+    //     var colorsCol = $(e.target);
+    //     // var colorInCol = colorsCol.children();
+
+    //     board.find($(".player1 .hole")).css("background", "white");
+    //     //.addClass(e.target.classList[1]);
+
+    //     console.log($(this).prop("class"));
+    //     console.log(e);
+    //     console.log(e.target);
+    //     console.log(e.target.classList[1]);
+    //     // console.log(currentPlayer);
+
+    //     // //  $(currentPlayer).addClass(e.target.classList[1]);
+    //     // currentPlayer = currentPlayer + " " + e.target.classList[1];
+    //     // //currentPlayer.removeClass
+
+    //     // preChip.removeClass("player1" && "player2");
+    //     // preChip.addClass(currentPlayer);
+    // });
+})();
+
+//HAMBURGER BUTTON & NAV
+(function hamburgerBtn() {
+    var nav = $("nav");
+    var btn1 = $(".open");
+    var btn2 = $(".continue");
+    var overlay = $(".overlay");
+
+    btn1.addClass("show");
+
+    btn1.on("click", function () {
+        nav.addClass("on");
+
+        overlay.css("visibility", "visible");
+        overlay.css("opacity", "50" + "%");
+        btn2.addClass("show");
+        btn1.removeClass("show");
+    });
+
+    btn2.on("click", function () {
+        nav.removeClass("on");
+        $(".colorsCont").removeClass("on");
+
+        overlay.css("opacity", 0);
+        overlay.css("visibility", "hidden");
+
+        btn2.removeClass("show");
+        btn1.addClass("show");
+    });
+})();
+
+////////////////////////////////
+
+// function checkDiag() {
+//     for (var i = 0; i < slots.length; i++) {
+//         if (
+//             slots.eq(i).hasClass(currentPlayer) &&
+//             slots.eq(i + 5).hasClass(currentPlayer) &&
+//             slots.eq(i + 10).hasClass(currentPlayer) &&
+//             slots.eq(i + 15).hasClass(currentPlayer)
+//         ) {
+//             console.log("rx Diagonal won!");
+//         } else if (
+//             slots.eq(i).hasClass(currentPlayer) &&
+//             slots.eq(i + 7).hasClass(currentPlayer) &&
+//             slots.eq(i + 14).hasClass(currentPlayer) &&
+//             slots.eq(i + 21).hasClass(currentPlayer)
+//         ) {
+//             console.log("sx Diagonal won!");
+//         }
+//     }
+// }
+
+// function checkDiag() {
+//     for (var i = 0; i < slots.length; i++) {
+//         var diagCounterRight = 0;
+//         if (slots.eq(i + 7).hasClass(currentPlayer)) {
+//             diagCounterRight++;
+//             console.log(diagCounterRight);
+//             if (diagCounterRight === 4) {
+//                 console.log("Diagonal won!");
+//                 return true;
+//             }
+//             // if (diagCounterRight < 4) {
+//             //     continue;
+//             //}
+//         }
+//     }
+// }
+
+// function checkDiag() {
+//     for (var i = 0; i < slots.length; i++) {
+//         // var diagCounterRight = 0;
+//         // var diagCounterLeft = 0;
+//         if (slots.eq(i).hasClass(currentPlayer)) {
+//             // console.log(slots.eq(i));
+
+//             diagCounterRight = slots.eq(i + 7);
+//             diagCounterLeft = slots.eq(i + 5);
+//             console.log(slots.eq(diagCounterRight));
+//             // if (
+//             //     slots.eq(diagCounterRight).hasClass(currentPlayer) ||
+//             //     slots.eq(diagCounterLeft).hasClass(currentPlayer)
+//             // ) {
+//             // }
+//             if (diagCounterRight === 22 || diagCounterLeft === 16) {
+//                 console.log("diagonal victory!!!");
+//                 return true;
+//             }
+//         }
+//     }
+// }
+
+function checkForVictory(slots) {
+    // console.log(slots);
+    var counter = 0; // number of adjacent chips in a column
+    for (var i = 0; i < slots.length; i++) {
+        if (slots.eq(i).hasClass(currentPlayer)) {
+            counter++;
+            if (counter === 4) {
+                return true;
+            }
+        } else if (
+            slots.eq(i).hasClass(currentPlayer) &&
+            slots.eq(i + 7).hasClass(currentPlayer) &&
+            slots.eq(i + 14).hasClass(currentPlayer) &&
+            slots.eq(i + 21).hasClass(currentPlayer) &&
+            ((((slots.eq(i).parent().index() + 1 ===
+                slots
+                    .eq(i + 7)
+                    .parent()
+                    .index()) ===
+                slots
+                    .eq(i + 7)
+                    .parent()
+                    .index() +
+                    1) ===
+                slots
+                    .eq(i + 14)
+                    .parent()
+                    .index()) ===
+                slots
+                    .eq(i + 14)
+                    .parent()
+                    .index() +
+                    1) ===
+                slots
+                    .eq(i + 21)
+                    .parent()
+                    .index()
+        ) {
+            return true;
+        } else if (
+            slots.eq(i).hasClass(currentPlayer) &&
+            slots.eq(i + 5).hasClass(currentPlayer) &&
+            slots.eq(i + 10).hasClass(currentPlayer) &&
+            slots.eq(i + 15).hasClass(currentPlayer) &&
+            ((((slots.eq(i).parent().index() + 1 ===
+                slots
+                    .eq(i + 5)
+                    .parent()
+                    .index()) ===
+                slots
+                    .eq(i + 5)
+                    .parent()
+                    .index() +
+                    1) ===
+                slots
+                    .eq(i + 10)
+                    .parent()
+                    .index()) ===
+                slots
+                    .eq(i + 10)
+                    .parent()
+                    .index() +
+                    1) ===
+                slots
+                    .eq(i + 15)
+                    .parent()
+                    .index()
+        ) {
+            return true;
+        } else {
+            counter = 0;
+            console.log("counter reset to 0 :(");
+        }
+    }
+}
