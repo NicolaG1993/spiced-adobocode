645cbfb6e38985994e81c05e18eb62d1ec10d01a
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-13 10:28:46 +0100

    express-practice cleaned up

---
+++ week6/express-practice/generateProjOverview.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const myPath = __dirname + "/projects";
+
+module.exports.projectOverviewHtml = function () {
+    //console.log("I will eventually generate html :)");
+    const content = fs.readdirSync(myPath, { withFileTypes: true });
+    let myFilesLinks = "";
+    for (let i = 0; i < content.length; i++) {
+        let fileLink = `<li><a href="/${content[i]["name"]}">${content[i]["name"]}</a></li>`;
+
+        myFilesLinks += fileLink;
+    }
+    myFilesLinks = `<ul>${myFilesLinks}</ul>`;
+
+    const myHtml = `<!doctype html>
+                    <html>
+                    <link rel="stylesheet" type="text/css" href="/style.css" />
+                    <title>ProjOverview</title>
+                    <div>
+                    <h1>WELCOME TO MY PORTFOLIO</h1>
+                    ${myFilesLinks}
+                    </div>
+                    </html>`;
+
+    return myHtml;
+};
