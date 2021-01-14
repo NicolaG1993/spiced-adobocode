d8a5142b1409576cfed531c09e225d9f68c7e2ce
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-14 17:32:30 +0100

    14.01

--- week6/hb-portfolio/index.js
+++ week6/hb-portfolio/index.js
@@ -13,16 +13,47 @@ app.get("/", (req, res) => {
     res.render("welcome", {
         layout: "main",
         cohort: "Adobo",
-        title: "welcome",
+        title: "HB Portfolio",
         myData,
+        //adding a local helper
+        // helpers: {
+        //     stressImportance(text) {
+        //         return text.toUpperCase() + "!!!!!" + 👻;
+        //     },
+        // },
     });
 });
 
 app.get("/about", (req, res) => {
     res.render("about", {
+        layout: "main",
         title: "about",
         emojis: ["🦩", "🚀", "👽"],
     });
+});
+
+app.get("/projects/:project", (req, res) => {
+    const project = req.params.project;
+    console.log("project: ", project);
+    // you can also use destructuring:
+    // const { project } = req.params;
+
+    const selectedProject = myData.find(
+        (item) => item.directory == project
+        //console.log("item: ", item);
+    );
+    //console.log("myData: ", myData);
+    console.log("selected project: ", selectedProject);
+
+    if (!selectedProject) {
+        return res.sendStatus(404);
+    } else {
+        res.render("description", {
+            layout: "main",
+            title: selectedProject.title,
+            description: selectedProject.description,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("hb portfolio listening on port 8080..."));
