7ebd998482be0f25c55b7fc413706e33d943055f
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-19 22:48:24 +0100

    19.01

--- week7/jest-testing/exercises/2/albums.test.js
+++ week7/jest-testing/exercises/2/albums.test.js
@@ -4,6 +4,10 @@ const spotify = require("./spotify");
 jest.mock("./spotify");
 
 test("album names are in alphabetical order", () => {
+    spotify.search.mockResolvedValue({
+        albums: { items: [{ name: "XXYXX" }, { name: "ACDC" }] },
+    });
+    //
     return getAlbumNames("meat loaf").then((albumNames) => {
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
