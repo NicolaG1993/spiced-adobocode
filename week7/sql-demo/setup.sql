54e741f8feb35037af5d866bd433ed64f9437366
Nicola Gaioni <nicog.designer@gmail.com>
2021-01-20 18:56:19 +0100

    20.01

---
+++ week7/sql-demo/setup.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS actors;
+
+CREATE TABLE actors (
+    "Name" VARCHAR,
+    "Age" INT,
+    "Number of Oscars" INT
+);
+
+INSERT INTO actors ("Name", "Age", "Number of Oscars") 
+VALUES ('Leonardo DiCaprio', 41, 1);
+INSERT INTO actors ("Name", "Age", "Number of Oscars") 
+VALUES ('Jennifer Lawrence', 25, 1);
+INSERT INTO actors ("Name", "Age", "Number of Oscars") 
+VALUES ('Samuel L. Jackson', 67, 0);
+INSERT INTO actors ("Name", "Age", "Number of Oscars") 
+VALUES ('Meryl Streep', 66, 3);
+INSERT INTO actors ("Name", "Age", "Number of Oscars") 
+VALUES ('John Cho', 43, 0);
+
+SELECT * FROM actors WHERE "Number of Oscars" > 1;
+SELECT * FROM actors WHERE "Age" > 30;
+/*
+
+psql -d actors
+
+SELECT * FROM actors;
+
+
+*/
+
+-- SELECT Oscars AS "Number of Oscars" FROM actors;
+
